aa41ace854a9cd48c14eb175fe20fae8b9328e68
vlad-lisnenko <lisnenkov777@gmail.com>
2023-07-17 23:16:03 +0300

    added Spring REST for employee

--- src/main/java/com/lisnenko/tasktracker/dao/EmployeeRepository.java
+++ src/main/java/com/lisnenko/tasktracker/dao/EmployeeRepository.java
@@ -2,9 +2,10 @@ package com.lisnenko.tasktracker.dao;
 
 import com.lisnenko.tasktracker.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
